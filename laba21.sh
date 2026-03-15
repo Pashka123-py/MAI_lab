7fb705c9e61b0764e44eb3f772982893f01840e0
Pashka123-py <pavel.yluanov@yandex.ru>
2026-03-16 00:08:39 +0300

    Update laba21.sh

--- laba21.sh
+++ laba21.sh
@@ -3,14 +3,15 @@
 # Определяем директорию, в которой находится сам скрипт
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# Формируем путь к искомой папке test
-TARGET_DIR="$SCRIPT_DIR/test_2"
+# Формируем путь к искомой папке test (название папки может быть любое, в данном случае программа выполняется над файлами папки test)
+TARGET_DIR="$SCRIPT_DIR/test"
 
 # В этой переменной можем задать произвольный размер для отбора файлов
 file_size=40  
 
 
 for file in "$TARGET_DIR"/*; do
+
     if [ -f "$file" ]; then
 
         size=$(stat -c%s "$file")
@@ -22,19 +23,21 @@ for file in "$TARGET_DIR"/*; do
             size_suff=${#suff}
             name_without_suff="${name%.*}"
             size_name_without_suff=${#name_without_suff}
+            
             if [ $size_name_without_suff -lt $size_suff ]; then
                 new_name="${name_without_suff}.${name_without_suff}"
+                
             else
-
+            
                 name_first_letter="${name:0:$size_suff}"
                 new_name="${name_without_suff}.${name_first_letter}"
-                # echo "$new_name"
 
             fi
+            
             path_old=$(dirname "$file")
             new_path="${path_old}/${new_name}"
             echo "$new_path"
-            mv "$file" "$new_path"
+            mv "$file" "$new_path"  
             
         fi
     fi
